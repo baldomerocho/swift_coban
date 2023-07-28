a606c439c7c96a5f9c761298a0d3f81d513b0193
Baldomero Cho <don.baldo@hotmail.com>
2023-07-28 16:53:20 -0600

    Initial Commit

---
+++ coban/cobanApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobanApp.swift
+//  coban
+//
+//  Created by Baldomero Cho on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct cobanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
